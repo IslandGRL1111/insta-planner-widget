bdf475e3b861932a33d6d8269f7145a8b2ce025f
IslandGRL1111 <cippolinaki@gmail.com>
2026-02-07 03:14:35 +0300

    Add homepage

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "system-ui",
+      }}
+    >
+      <h1>✨ Insta Planner Widget is alive ✨</h1>
+    </div>
+  );
+}
