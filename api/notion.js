4d76dcdc38d4d2491d08ccc795817c83c9855150
IslandGRL1111 <cippolinaki@gmail.com>
2026-02-14 00:06:18 +0300

    Update notion.js

--- api/notion.js
+++ api/notion.js
@@ -13,11 +13,21 @@ export default async function handler(req, res) {
 
   const json = await response.json();
 
-  const posts = json.results.map(p => ({
-  platformRaw: p.properties["Platform Name"]?.multi_select,
-  statusRaw: p.properties["Status"]?.status?.name,
-  showRaw: p.properties["Show in Widget"]?.checkbox
-}));
+  const posts = json.results
+  .filter(p =>
+    p.properties["Show in Widget"]?.checkbox === true &&
+    p.properties["Platform Name"]?.multi_select?.some(x => x.name === "🩷Instagram") &&
+    p.properties["Status"]?.status?.name === "🕒Scheduled"
+  )
+  .map(p => ({
+    id: p.id,
+    pinned: p.properties["Pin Post?"]?.checkbox || false,
+    title: p.properties["Content Title/ Caption/ Hook"]?.title?.[0]?.plain_text || "",
+    date: p.properties["Scheduled Date & Time"]?.date?.start || null,
+    platform: p.properties["Platform Name"]?.multi_select?.map(x=>x.name) || [],
+    type: p.properties["Type of Post"]?.multi_select?.map(x=>x.name) || [],
+    images: p.properties["Post Preview"]?.files?.map(f => f.file?.url) || [],
+  }));
 
   res.status(200).json(posts);
 }
